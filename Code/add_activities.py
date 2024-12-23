10df0e8a39587e2b446a930c78d69925fb57132a
colasmar <h.grandjean@afdec.fr>
2024-12-23 16:27:52 +0100

    html affichant les activités en volet déroulant

---
+++ Code/add_activities.py
@@ -0,0 +1,13 @@
+from extensions import db
+from models.models import Activity
+from app import app
+
+with app.app_context():
+    activities = [
+        Activity(name="Activité 1"),
+        Activity(name="Activité 2"),
+        Activity(name="Activité 3")
+    ]
+    db.session.add_all(activities)
+    db.session.commit()
+    print("Activités ajoutées avec succès !")
--- Code/routes/ui_routes.py
+++ Code/routes/ui_routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template
+from models.models import Activity
 
 ui_bp = Blueprint('ui', __name__, url_prefix='/ui')
 
 @ui_bp.route('/activities', methods=['GET'])
 def activities():
-    return render_template('ui/activities.html')
+    # Récupérer toutes les activités depuis la base de données
+    activities = Activity.query.all()
+    return render_template('ui/activities.html', activities=activities)
